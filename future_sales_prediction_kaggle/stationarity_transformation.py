2e83eb31e06a2e7aa3d200fa952c6e39d906c8ff
andrewcreates <wer345erty@gmail.com>
2020-08-06 10:16:25 -0700

    initial commit

---
+++ future_sales_prediction_kaggle/stationarity_transformation.py
@@ -0,0 +1,19 @@
+def test_stationarity(timeseries):
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+
+#Create a differenced series to remove trend and seasonality
+def difference(dataset, interval=1):
+    diff = list()
+    for i in range(interval, len(dataset)):
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+    return pd.Series(diff)
+
+# invert differenced forecast
+def inverse_difference(last_ob, value):
+    return value + last_ob
